c75f8ceb59e38aa88d8fac51cb7872e965bfc182
Andressa Agostini <andressa.agostini@tecnovaenergia.com.br>
2026-04-10 11:25:11 -0300

    Add OpenID login button to SocialLoginRender

--- client/src/components/Auth/SocialLoginRender.tsx
+++ client/src/components/Auth/SocialLoginRender.tsx
@@ -81,23 +81,29 @@ function SocialLoginRender({
         id="apple"
       />
     ),
-openid: startupConfig.openidLoginEnabled
-    saml: startupConfig.samlLoginEnabled && (
-      <SocialButton
-        key="saml"
-        enabled={startupConfig.samlLoginEnabled}
-        serverDomain={startupConfig.serverDomain}
-        oauthPath="saml"
-        Icon={() =>
-          startupConfig.samlImageUrl ? (
-            <img src={startupConfig.samlImageUrl} alt="SAML Logo" className="h-5 w-5" />
-          ) : (
-            <SamlIcon />
-          )
-        }
-        label={startupConfig.samlLabel ? startupConfig.samlLabel : localize('com_auth_saml_login')}
-        id="saml"
-      />
+openid: startupConfig.openidLoginEnabled && (
+      <div key="openid">
+        <SocialButton
+          enabled={startupConfig.openidLoginEnabled}
+          serverDomain={startupConfig.serverDomain}
+          oauthPath="openid"
+          Icon={() => (
+            <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21">
+              <rect x="1" y="1" width="9" height="9" fill="#f25022" />
+              <rect x="11" y="1" width="9" height="9" fill="#7fba00" />
+              <rect x="1" y="11" width="9" height="9" fill="#00a4ef" />
+              <rect x="11" y="11" width="9" height="9" fill="#ffb900" />
+            </svg>
+          )}
+          label="Entrar com Microsoft"
+          id="openid"
+        />
+        <p className="mt-3 text-center text-sm text-gray-500 dark:text-gray-400">
+          Use sua conta corporativa da Tecnova
+          <br />
+          para acessar o sistema.
+        </p>
+      </div>
     ),
   };
 
